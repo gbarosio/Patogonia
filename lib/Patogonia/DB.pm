aa089ef431fb3170f0710559e72223a88191fe68
Guido Barosio <gbarosio@gmail.com>
2015-10-19 19:10:52 -0300

    meh!

--- lib/Patogonia/DB.pm
+++ lib/Patogonia/DB.pm
@@ -3,33 +3,27 @@ package Patogonia::DB;
 use 5.018002;
 use strict;
 use warnings;
+use DBI;
 
 require Exporter;
 use AutoLoader qw(AUTOLOAD);
 
 our @ISA = qw(Exporter);
 
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Patogonia::DB ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
 our @EXPORT = qw(
-	
+	connect_db
+	execute	
 );
 
 our $VERSION = '0.01';
 
+sub connect_db($) {
+	return 1;
+}
 
-# Preloaded methods go here.
+sub execute($) {
+	return 1;
+}
 
 # Autoload methods go after =cut, and are processed by the autosplit program.
 
